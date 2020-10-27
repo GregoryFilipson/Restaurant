961984d24dac0f0ad4816c2c02507386e50ce0e3
GregoryFilipson <alivenemo@gmail.com>
2020-10-27 19:06:10 +0300

    fixed bugs

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -3,9 +3,9 @@ import java.util.Queue;
 
 public class Restaurant {
     private final Queue<Guest> guestQueue = new LinkedList<>();
-    private final int COOKING_TIME = 3000;
-    private final int TIME_TO_COME_THE_GUEST = 300;
-    private final int TIME_FOR_EATING = 1500;
+    private final int COOKING_TIME = 2000;
+    private final int TIME_TO_COME_THE_GUEST = 1500;
+    private final int TIME_FOR_EATING = 1000;
     private final int TIME_BETWEEN_GUESTS = 1000;
 
     public void guestIsHere() {
@@ -14,14 +14,13 @@ public class Restaurant {
                 Thread.sleep(TIME_BETWEEN_GUESTS);
                 System.out.println(Thread.currentThread().getName() + " в ресторане!");
                 guestQueue.offer(new Guest());
-                notify();
+                notify(); //должен быть в конце! Сделать 2 блока синхронизации?
+                wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-
         try {
-            wait();
             Thread.sleep(COOKING_TIME + TIME_TO_COME_THE_GUEST);
             System.out.println(Thread.currentThread().getName() + " приступил к еде");
             Thread.sleep(TIME_FOR_EATING);
@@ -31,30 +30,28 @@ public class Restaurant {
         }
     }
 
-
     public void waiterIsWorking() {
+        System.out.println(Thread.currentThread().getName() + " на работе!");
         synchronized (this) {
-            while (guestQueue.size()<1) {
-                System.out.println(Thread.currentThread().getName() + " на работе!");
-                if (guestQueue.size() == 0) {
-                    try {
-                        wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                } else {
-                    try {
-                        System.out.println(Thread.currentThread().getName() + " взял заказ...");
-                        Thread.sleep(COOKING_TIME);
-                        guestQueue.poll();
-                        System.out.println(Thread.currentThread().getName() + " несет заказ. Наконец то!");
-                        Thread.sleep(TIME_TO_COME_THE_GUEST);
-                        notify();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+            if (guestQueue.size() == 0) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    System.out.println(Thread.currentThread().getName() + " взял заказ...");
+                    Thread.sleep(COOKING_TIME);
+                    guestQueue.poll();
+                    System.out.println(Thread.currentThread().getName() + " несет заказ. Наконец то!");
+                    Thread.sleep(TIME_TO_COME_THE_GUEST);
+                    notify();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         }
     }
 }
+
