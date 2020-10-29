f673e2a2c1ea2e9e685a0d89c77cb3da093cc42e
GregoryFilipson <alivenemo@gmail.com>
2020-10-29 08:06:07 +0300

    fixed problems

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -3,55 +3,63 @@ import java.util.Queue;
 
 public class Restaurant {
     private final Queue<Guest> guestQueue = new LinkedList<>();
+    private final Object lock = new Object();
     private final int COOKING_TIME = 2000;
-    private final int TIME_TO_COME_THE_GUEST = 1500;
+    private final int TIME_TO_COME_THE_GUEST = 300;
     private final int TIME_FOR_EATING = 1000;
-    private final int TIME_BETWEEN_GUESTS = 1000;
+    private final int TIME_BETWEEN_GUESTS = 1100;
+    private final int LIMIT_GUESTS = 5;
 
     public void guestIsHere() {
-        synchronized (this) {
+        synchronized (guestQueue) {
             try {
                 Thread.sleep(TIME_BETWEEN_GUESTS);
                 System.out.println(Thread.currentThread().getName() + " в ресторане!");
                 guestQueue.offer(new Guest());
-                notify(); //должен быть в конце! Сделать 2 блока синхронизации?
-                wait();
+                guestQueue.notify();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        try {
-            Thread.sleep(COOKING_TIME + TIME_TO_COME_THE_GUEST);
-            System.out.println(Thread.currentThread().getName() + " приступил к еде");
-            Thread.sleep(TIME_FOR_EATING);
-            System.out.println(Thread.currentThread().getName() + " вышел из ресторана.");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        synchronized (lock) {
+            try {
+                lock.wait();
+                System.out.println(Thread.currentThread().getName() + " приступил к еде");
+                Thread.sleep(TIME_FOR_EATING);
+                System.out.println(Thread.currentThread().getName() + " вышел из ресторана.");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void waiterIsWorking() {
         System.out.println(Thread.currentThread().getName() + " на работе!");
-        synchronized (this) {
+        while (guestQueue.size() <= LIMIT_GUESTS) {
             if (guestQueue.size() == 0) {
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                synchronized (guestQueue) {
+                    try {
+                        guestQueue.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
-            } else {
-                try {
-                    System.out.println(Thread.currentThread().getName() + " взял заказ...");
-                    Thread.sleep(COOKING_TIME);
-                    guestQueue.poll();
-                    System.out.println(Thread.currentThread().getName() + " несет заказ. Наконец то!");
-                    Thread.sleep(TIME_TO_COME_THE_GUEST);
-                    notify();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " взял заказ...");
+            try {
+                Thread.sleep(COOKING_TIME);
+                guestQueue.poll();
+                System.out.println(Thread.currentThread().getName() + " несет заказ. Наконец то!");
+                Thread.sleep(TIME_TO_COME_THE_GUEST);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (lock) {
+                lock.notify();
                 }
+            if (guestQueue.size()==0) {
+                break;
             }
         }
     }
 }
-
